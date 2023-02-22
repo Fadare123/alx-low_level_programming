bda399acda201dc1ecade14a46eeedee4608add6
Fadare123 <florencefadare99@gmail.com>
2023-02-21 20:44:39 -0800

    help

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+#define main_h
 
 int _putchar(char);
 void print_alphabet(void);
@@ -13,6 +13,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-int _putchar(char c);
+void print_times_table(int n);
 
 #endif
+
